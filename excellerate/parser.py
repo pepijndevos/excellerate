586b8097813f119747ad3a60fb4dd2897ae16034
Pepijn de Vos <pepijndevos@gmail.com>
2020-08-15 12:24:22 +0200

    add parser tests

--- excellerate/parser.py
+++ excellerate/parser.py
@@ -4,6 +4,7 @@ from openpyxl.formula import Tokenizer
 from parsy import regex, generate, test_item, string, seq, fail
 from dataclasses import dataclass
 from typing import List, Tuple, Any
+import unittest
 
 def test_token(typ, subtype=None, value=None):
     def tester(token):
@@ -101,14 +102,36 @@ def simple():
 
 expr = literal | precedence(simple, exp_t, mult_t, add_t, conc_t, comp_t)
 
-def parse(tokens):
-    return expr.parse(tokens)
+def parse(formula):
+    tokenizer = Tokenizer(formula)
+    return expr.parse(tokenizer.items)
 
+
+#### TESTS ####
+
+class TestQ(unittest.TestCase):
+
+    def test_literal(self):
+        self.assertEqual(parse("-4e3"), -4e3)
+        self.assertEqual(parse("foo"), None)
+
+    def test_number(self):
+        self.assertEqual(parse("=-4e3"), -4e3)
+
+    def test_range(self):
+        self.assertEqual(parse("=$A$4"), Range(sheet=None, boundaries=(1, 4, 1, 4)))
+        self.assertEqual(parse("=A4:B5"), Range(sheet=None, boundaries=(1, 4, 2, 5)))
+        self.assertEqual(parse("=sheet!A4:B5"), Range(sheet='sheet', boundaries=(1, 4, 2, 5)))
+        self.assertEqual(parse("='my sheet'!A4:B5"), Range(sheet='my sheet', boundaries=(1, 4, 2, 5)))
+
+    def test_array(self):
+        self.assertEqual(parse("={1,2,3;4,5,6;7,8,9}"), Array([[1,2,3],[4,5,6],[7,8,9]]))
+
+    def test_func(self):
+        self.assertEqual(parse("=MAX(1,2,3)"), Function("MAX", [1,2,3]))
+
+    def test_op(self):
+        self.assertEqual(parse("=5>1+2*3^-4"), ('>', 5.0, ('+', 1.0, ('*', 2.0, ('^', 3.0, -4.0)))))
 
 if __name__ == '__main__':
-    from pprint import pprint
-    #tokenizer = Tokenizer("""=IF($A$1,1+2*3,MAX(3+4/5,'Sheet 2'!B1))""")
-    #tokenizer = Tokenizer("""=(5%>-1)^A2+2*3*{1,2;3,4}+MAX(1,2,3)""")
-    tokenizer = Tokenizer("""=A100:F9""")
-
-    pprint(parse(tokenizer.items))
+    unittest.main() 
